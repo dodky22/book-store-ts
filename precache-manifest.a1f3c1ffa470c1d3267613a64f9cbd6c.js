8db5db0d95b45caa63ba021f948b0db8346de468
dodky <dodkymull@gmail.com>
2020-09-05 10:31:22 +0200

    Updates

---
+++ precache-manifest.a1f3c1ffa470c1d3267613a64f9cbd6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550de280cc99eb7726c82428425ee0d6",
+    "url": "/book-store-ts/index.html"
+  },
+  {
+    "revision": "c1dfaf6ddd2acca4d066",
+    "url": "/book-store-ts/static/css/main.bee35328.chunk.css"
+  },
+  {
+    "revision": "dd5a591fc2df158f41b1",
+    "url": "/book-store-ts/static/js/2.63377925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-store-ts/static/js/2.63377925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1dfaf6ddd2acca4d066",
+    "url": "/book-store-ts/static/js/main.c19899fe.chunk.js"
+  },
+  {
+    "revision": "03e48e675ecd186c7438",
+    "url": "/book-store-ts/static/js/runtime-main.d40f7f39.js"
+  },
+  {
+    "revision": "47b49276e2dcd396b02fcefc03f46466",
+    "url": "/book-store-ts/static/media/loading-gear.47b49276.gif"
+  },
+  {
+    "revision": "dbf242a8521dba51a031eb2d7fc78980",
+    "url": "/book-store-ts/static/media/title-books.dbf242a8.PNG"
+  }
+]);
